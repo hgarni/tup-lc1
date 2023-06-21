2e2667e544231d5de316c8b848c74b4b0fb25159
hgarni <hgarni@hotmail.com>
2023-06-21 20:35:30 -0300

    Resolucion Ejercicio 1

--- Practica-6/P6_E1.c
+++ Practica-6/P6_E1.c
@@ -1,1 +1,36 @@
-//Ejercicio 1 de la prctica 6 de funciones
+//Ejercicio 1 de la practica 6 de funciones
+#include <stdio.h> 
+
+float cubica(float numero) 
+{ 
+    float cubica; 
+    cubica = numero * numero * numero;
+    return cubica;
+}
+
+int main()
+{ 
+    /* Crear una función que calcule el cubo de un número real (float). 
+     * El resultado deberá ser otro número real. 
+     * Probar esta función para calcular el cubo de 3.2 y el de 5. */
+
+    float numero; 
+    float cubo; 
+    int i=0; 
+
+    while(numero >= -1000000) 
+    { 
+        if(i==0) { 
+            printf("\nEscriba un numero real"); 
+        } 
+        else { 
+            printf("\nEscriba otro numero real"); 
+        } 
+        
+        scanf("%f", &numero); 
+        printf("%f elevado al cubo es %f\n", numero,cubo=cubica(numero)); 
+        i ++;
+    } 
+            
+    return 0;
+}
